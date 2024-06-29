b0133fe85234426b2955257666326b59b8a4ca35
Evan Gyori <evangyori@gmail.com>
2024-06-29 13:22:21 -0700

    Each round of the battle now occurs over multiple updates rather than one.
    Also added comments to EnemyTile class.

--- Tiles/EnemyTile.cpp
+++ Tiles/EnemyTile.cpp
@@ -6,11 +6,9 @@ EnemyTile::~EnemyTile()
 
 void EnemyTile::update(TileMediator& mediator)
 {
-	while (mediator.isPlayerAlive() && getHp() > 0) {
-		handlePlayersTurn(mediator);
-		handleTilesTurn(mediator);
-	}
-	
+	handlePlayersTurn(mediator);
+	handleTilesTurn(mediator);
+		
 	if (getHp() <= 0) {
 		handleTilesDeath(mediator);
 	}
--- Tiles/EnemyTile.h
+++ Tiles/EnemyTile.h
@@ -12,6 +12,7 @@ class EnemyTile : public Tile
 public:
 	virtual ~EnemyTile();
 
+	// Controls the battle loop between the player and tile
 	virtual void update(TileMediator&);
 	
 	virtual void changeHp(int deltaHp) = 0;
@@ -20,12 +21,27 @@ public:
 	virtual int getHp() const = 0;
 	virtual int getMaxHp() const = 0;
 	virtual int getDamage() const = 0;
+	
+	/*
+	 Returns a string listing all of the special abilities of the tile.
+	 These abilities are determined by the enemy tile decorators
+	*/
 	virtual std::string getAbilities() const = 0;
 	
 protected:
+	// Handles the beginning of a battle with a player's input.
 	virtual void handlePlayersTurn(TileMediator&) = 0;
+	
+	// Handles after the player gives input and now its the tile's turn to deal damage.
 	virtual void handleTilesTurn(TileMediator&) = 0;
+	
 	virtual void handleTilesDeath(TileMediator&) = 0;
 	
+	/*
+	 Each decorator must call this on its aggregate on instantiation.
+	 When this is called on a decorator, it must pass the root to its aggregate.
+	 The root is held so that an instance passes the root rather than itself to the TileMediator
+	 so that polymorphism may properly be performed.
+	*/
 	virtual void setRootParent(EnemyTile* root) = 0;
 };
